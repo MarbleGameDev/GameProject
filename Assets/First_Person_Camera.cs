ec4946d2be401b2656371828e9491b522e2f41d1
Uberpangolin <jolewis@student.csdspartans.org>
2015-08-10 21:30:30 -0400

    Unity didnt add this to the last one for some reason
    Yeah unity can be dumb

---
+++ Assets/First_Person_Camera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class First_Person_Camera : MonoBehaviour 
+{
+	public GameObject target = null;
+
+	void Start () 
+	{
+	
+	}
+	
+
+	void Update () 
+	{	//ok
+		//transform.position = target.transform.position;
+		//stransform.LookAt (target);
+	}
+
+
+
+}
