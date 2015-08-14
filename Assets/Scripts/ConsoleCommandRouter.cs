807bde9501851523777b5df965d65160860f67a0
thejereman13 <jereman13@gmail.com>
2015-08-13 23:07:55 -0400

    Beginning work on the Menu System
    Got the main menu layout started alongside background image and worked
    on button functionality. Also fixed the sprint key to shift from
    control. Also added an exit command to console.

--- Assets/Scripts/ConsoleCommandRouter.cs
+++ Assets/Scripts/ConsoleCommandRouter.cs
@@ -8,11 +8,17 @@ public class ConsoleCommandRouter : MonoBehaviour {
 		var repo = ConsoleCommandsRepository.Instance;
 		repo.RegisterCommand("help", Help);
 		repo.RegisterCommand("load", Load);
+		repo.RegisterCommand ("exit", Exit);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public string Exit(params string[] args){
+		Application.Quit ();
+		return "Closing Game";
 	}
 
 	public string Help(params string[] args){
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour {
+	
+	public void ChangeToScene (string Scene) {
+		Application.LoadLevel (Scene);
+	}
+}
