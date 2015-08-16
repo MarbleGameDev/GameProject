90dd4db5bdae4a8abd238fd16f7c686ba4150b0e
Uberpangolin <jolewis@student.csdspartans.org>
2015-08-16 10:27:15 -0400

    Revert "Main Menu Options Work"
    This reverts commit 02505ba4d0ccec90e767cbf8d94c84ce5fa1a1ab.

---
+++ Assets/Scripts/SetQuality.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetQuality : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Slider qualitySlider = GetComponent<Slider> ();
+		qualitySlider.value = (float) QualitySettings.GetQualityLevel();
+		print (QualitySettings.GetQualityLevel ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void setGraphicsQuality(){
+		Slider qualitySlider = GetComponent<Slider> ();
+		switch ((int)qualitySlider.value) {
+		case 1:
+			QualitySettings.SetQualityLevel(0, true);
+			break;
+		case 2:
+			QualitySettings.SetQualityLevel(1, true);
+			break;
+		case 3:
+			QualitySettings.SetQualityLevel(2, true);
+			break;
+		case 4:
+			QualitySettings.SetQualityLevel(3, true);
+			break;
+		case 5:
+			QualitySettings.SetQualityLevel(4, true);
+			break;
+		case 6:
+			QualitySettings.SetQualityLevel(5, true);
+			break;
+		}
+		int quality = QualitySettings.GetQualityLevel();
+		print (quality);
+	}
+}
