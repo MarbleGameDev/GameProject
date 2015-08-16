02505ba4d0ccec90e767cbf8d94c84ce5fa1a1ab
thejereman13 <jereman13@gmail.com>
2015-08-15 23:16:12 -0400

    Main Menu Options Work
    Made resolution dropdown menu and different option panels to toggle
    between. Still working on scaling with different resolutions.

---
+++ Assets/Scripts/GUI/Main Menu/ControlSettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlSettings : MonoBehaviour {
+	GameObject optype;
+	OptionsType options;
+	// Use this for initialization
+	void Start () {
+		gameObject.SetActive (false);
+		optype = GameObject.Find ("Optiontypes");
+		options = optype.GetComponent<OptionsType> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (options.controlsEnabled) {
+			gameObject.SetActive (true);
+		} else {
+			gameObject.SetActive (false);
+		}
+	}
+}
